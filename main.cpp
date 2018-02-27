ad3ff145aed547346fb1e6475eb2f290a33e2f0f
yangx3 <yxy100good@gmail.com>
2018-02-26 21:25:14 -0800

    change logic error

--- main.cpp
+++ main.cpp
@@ -26,26 +26,28 @@ Log idea comes from website.
 
 using namespace std;
 
-//changes: using typedef struct instead of struct from A3 for both dep and emp
-typedef struct Dept 
+//changes: struct from A3 for both dep and emp, values type match them, not only string applied
+struct Dept 
 {
 	long int managerId;
     long int did;
     char dname[40];
     double budget;
    
-} Dept;
+};
 
-typedef struct Emp
+//emp struct
+struct Emp
 {
     long int eid;
     char ename[40];
     long int age;
     double salary;
     
-} Emp;
+};
 
-typedef struct EmpDep
+//for join strcut
+struct EmpDep
 {
     long int did;
     long int eid;
@@ -55,10 +57,10 @@ typedef struct EmpDep
     double salary;
 	long int age;
     long int managerId;    
-} EmpDep;
+};
 
-int LogNum = 0;
-const bool TEST_CRASH = true;  //set to false if dont want to do crash testing
+//int LogNum = 0;
+bool TEST_CRASH = true;  //set to false if dont want to do crash testing
 int State = 50; //1/50 to crash/in testing, it is 1/20
 
 void outPutLog(FILE* log, int depPre, int empPre, int output);
@@ -68,12 +70,25 @@ Dept* getDep(FILE* depFile);
 
 
 //using another way for join algorithm
-typedef struct SortTemp{
+struct SortTemp{
 	//int num;  Using for debugging. delete it
 	int dep;
 	int emp;
 	int join;
-}SortTemp;
+};
+
+
+//error check functions, check then open file->
+FILE* CheckOpenFile(string filename, string args){
+	FILE* fp = fopen(filename.c_str(), args.c_str());
+	if(!fp){
+		cout << "Error found, can't read/write file "<< filename << endl;
+		exit(1);
+		
+	}
+	return fp;
+}
+
 
 
 void testCrash(){
@@ -103,17 +118,6 @@ SortTemp* readLog(FILE* log){
 	
 }
 
-//error check functions, check then open file->
-FILE* CheckOpenFile(string filename, string args){
-	FILE* fp = fopen(filename.c_str(), args.c_str());
-	if(!fp){
-		cout << "Error found, can't read/write file "<< filename << endl;
-		exit(1);
-		
-	}
-	return fp;
-}
-
 //check if log file or read file exist AND can be read. if join file exist but empty, concerned as "no recording"
 bool LogCheck(string filename){
 	bool check_result;
@@ -123,6 +127,68 @@ bool LogCheck(string filename){
 		check_result = fgetc(fp) != EOF;
 		fclose(fp);}
 	return check_result;
+}
+
+
+//create new line to log. In Order: Department, Emp and join's line number
+void outPutLog(FILE* log, int depPre, int empPre, int output){
+	if(!log)
+		return;
+	SortTemp temp;
+	//temp.num = LogNum;
+	temp.dep = depPre;
+	temp.emp = empPre;
+	temp.join = output;
+	writeLog(log, &temp);
+	
+}
+
+
+//recover from last run's position->
+void recover(FILE* log, FILE* depFile, FILE* empFile, FILE* outFile){
+	fseek(log, 0, SEEK_END);
+	int position = ftell(log);
+	cout << "restart at position: " << position << endl;
+	int num = 0;
+	int output;
+	char character;
+	fseek(log, position, SEEK_SET);
+	//cout << "Test point 1" << endl;
+	while(num < 2){
+		output = fgetc(log);
+		character = output;
+		if(character == '\n')
+			num+=1;
+		position -= 1;
+		fseek(log, position, SEEK_SET);
+	}
+	fseek(log, position + 2, SEEK_SET);
+	SortTemp* temp = readLog(log);
+	//temp->num = LogNum;
+	fseek(depFile, temp->dep, SEEK_SET);
+	fseek(empFile, temp->emp, SEEK_SET);
+	fseek(outFile, temp->join, SEEK_SET);
+	delete temp;
+}
+
+
+//New, easy way to read dept and emp
+Dept* getDep(FILE* file){
+	Dept* temp = new Dept;
+	if(!feof(file))
+		fscanf(file, "\"%ld\",\"%[^\"]\",\"%la\",\"%ld\"\n", &temp->did, temp->dname, &temp->budget, &temp->managerId);
+	else
+		temp = NULL;
+    return temp;
+}
+
+Emp* getEmp(FILE* file){
+	Emp* temp = new Emp;
+	if(!feof(file))
+		fscanf(file, "\"%ld\",\"%[^\"]\",\"%ld\",\"%la\"\n", &temp->eid, temp->ename, &temp->age, &temp->salary);
+	else
+		temp = NULL;
+	return temp;
 }
 
 //another way to write on files, reference: stack overflow
@@ -171,7 +237,6 @@ void Mergejoin(FILE* outFile, FILE* depFile, FILE* empFile, FILE* log){
 				DepPre = ftell(depFile);
 				dep = getDep(depFile);
 				outPutLog(log, DepPre, EmpPre, ftell(outFile));
-				testCrash();
 			}
 			else{
 				delete emp;
@@ -179,9 +244,9 @@ void Mergejoin(FILE* outFile, FILE* depFile, FILE* empFile, FILE* log){
 				EmpPre = ftell(empFile);
 				emp = getEmp(empFile);
 				outPutLog(log, DepPre, EmpPre, ftell(outFile));
-				testCrash();
 			}
 		}
+		testCrash();
 		if(emp == NULL || dep == NULL){
 			Done = true;
 			break;}
@@ -190,66 +255,6 @@ void Mergejoin(FILE* outFile, FILE* depFile, FILE* empFile, FILE* log){
 	
 	}
 	cout << "Finish at " << timeout << endl; // testing used.
-}
-
-//create new line to log. In Order: Department, Emp and join's line number
-void outPutLog(FILE* log, int depPre, int empPre, int output){
-	if(!log)
-		return;
-	SortTemp temp;
-	//temp.num = LogNum;
-	temp.dep = depPre;
-	temp.emp = empPre;
-	temp.join = output;
-	writeLog(log, &temp);
-	
-}
-
-//recover from last run's position->
-void recover(FILE* log, FILE* depFile, FILE* empFile, FILE* outFile){
-	fseek(log, 0, SEEK_END);
-	int position = ftell(log);
-	cout << "restart at position: " << position << endl;
-	int num = 0;
-	int output;
-	char character;
-	fseek(log, position, SEEK_SET);
-	//cout << "Test point 1" << endl;
-	while(num < 2){
-		output = fgetc(log);
-		character = output;
-		if(character == '\n')
-			num+=1;
-		position -= 1;
-		fseek(log, position, SEEK_SET);
-	}
-	fseek(log, position + 2, SEEK_SET);
-	SortTemp* temp = readLog(log);
-	//temp->num = LogNum;
-	fseek(depFile, temp->dep, SEEK_SET);
-	fseek(empFile, temp->emp, SEEK_SET);
-	fseek(outFile, temp->join, SEEK_SET);
-	delete temp;
-}
-
-
-//New, easy way to read dept and emp
-Dept* getDep(FILE* file){
-	Dept* temp = new Dept;
-	if(!feof(file))
-		fscanf(file, "\"%ld\",\"%[^\"]\",\"%la\",\"%ld\"\n", &temp->did, temp->dname, &temp->budget, &temp->managerId);
-	else
-		temp = NULL;
-    return temp;
-}
-
-Emp* getEmp(FILE* file){
-	Emp* temp = new Emp;
-	if(!feof(file))
-		fscanf(file, "\"%ld\",\"%[^\"]\",\"%ld\",\"%la\"\n", &temp->eid, temp->ename, &temp->age, &temp->salary);
-	else
-		temp = NULL;
-	return temp;
 }
 
 //Move EMPDEP from main (which I have done in Assignment 3), require dep and emp not NULL #this is important!!! 
